6dbe42ebd31496331458dde13ca6c5d837299c85
David Keller <d.keller@oodrive.com>
2020-04-12 01:19:16 +0200

    update upload design

--- src/app/mailform/mailform.component.ts
+++ src/app/mailform/mailform.component.ts
@@ -118,12 +118,10 @@ export class MailformComponent {
     }
     const k = 1024;
     const dm = decimals <= 0 ? 0 : decimals || 2;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const sizes = ['Bytes', 'kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
-
-
 
   constructor(private fb: FormBuilder) {
     this.filteredRecipients = this.recipientCtrl.valueChanges.pipe(
